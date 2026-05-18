cb99ff0fcf502ea76d214aca3324cc3eee24ef23
Aline NIYONIZERA <niyocroirealine@gmail.com>
2026-05-18 18:14:41 +0200

    Update s_curve_painter.dart

--- mobile/lib/widgets/s_curve_painter.dart
+++ mobile/lib/widgets/s_curve_painter.dart
@@ -1,24 +1,43 @@
 import 'package:flutter/material.dart';
 
 class SCurvePainter extends CustomPainter {
-  final Color color;
-
-  SCurvePainter({required this.color});
 
   @override
   void paint(Canvas canvas, Size size) {
+
+    // Paint object controls color and style
     final paint = Paint()
-      ..color = color
+      ..color = Colors.green // Background curve color
       ..style = PaintingStyle.fill;
-    
+
+    // Path is used to draw custom shapes
     Path path = Path();
+
+    // Starting point
     path.moveTo(0, size.height * 0.2);
-    path.cubicTo(size.width * 0.5, size.height * 0.1, size.width * 0.5, size.height * 0.4, size.width, size.height * 0.3);
+
+    // Creates the S-like curve
+    path.cubicTo(
+      size.width * 0.5,  // control point 1 x
+      size.height * 0.1, // control point 1 y
+
+      size.width * 0.5,  // control point 2 x
+      size.height * 0.4, // control point 2 y
+
+      size.width,        // ending point x
+      size.height * 0.3, // ending point y
+    );
+
+    // Complete the bottom shape
     path.lineTo(size.width, size.height);
     path.lineTo(0, size.height);
+
+    // Draw the shape on screen
     canvas.drawPath(path, paint);
   }
 
   @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
 }
